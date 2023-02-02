850e0932b47825bed7e1854242d583745b6a6a02
Peter Širka <petersirka@gmail.com>
2023-02-02 20:47:10 +0100

    Fixed icon.

--- definitions/init.js
+++ definitions/init.js
@@ -4,6 +4,8 @@ MAIN.paused = true;
 MAIN.ready = false;
 
 CONF.allow_custom_titles = true;
+CONF.version = '1';
+CONF.op_icon = 'ti ti-database';
 
 // UI
 COMPONENTATOR('ui', 'layout,properties,searchinput,search,columns,notify,fileuploader,empty,dropdownlist,message,edit,datepicker,clipboard,datagrid,windows,flow,menu,movable,intranetcss,radiobuttonexpert,exec,input,colorpicker,icons,box,miniform,viewbox,directory,loading,locale,importer,part,validate,approve,tangular-jsonformat,tangular-color,objecttree,errorhandler,ready');
